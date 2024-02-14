4994a6db901af32f166b2c5a53d7af7802f6c851
Ünal <ucarmustafaunal@gmail.com>
2024-02-14 13:47:46 +0300

    final_keyword coursework comitment

---
+++ access_modifiers/final_keyword/src/Circle.java
@@ -0,0 +1,13 @@
+public class Circle {
+    public int r;
+    public final double PI_NUMBER = 3.14;
+
+    public Circle(int r){
+        this.r = r;
+    }
+
+    public void calcArea(){
+        double area = PI_NUMBER * this.r * this.r;
+        System.out.println("Area of the Circle is : "+area);
+    }
+}
---
+++ access_modifiers/final_keyword/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle c = new Circle(5);
+        c.calcArea();
+    }
+}
