5bde47eb5611a21e7fb93bbeba82097bd936c1fd
Ünal <ucarmustafaunal@gmail.com>
2024-02-14 15:23:23 +0300

    encapsulation section's commitment

---
+++ oop_principles/encapsulation/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Book harrypotter = new Book("Harry Potter", 150);
+        Book lordoftherings = new Book("Lord of the Rings", -250);
+        //lordoftherings.pageNumber = -100;
+        lordoftherings.setPageNumber(-100);
+
+        //System.out.println(lordoftherings.pageNumber);
+        //System.out.println(lordoftherings.name);
+
+        System.out.println(lordoftherings.getName());
+        System.out.println(lordoftherings.getPageNumber());
+    }
+}
